8adc0c86c0f004814b59186573d404ed083cab61
Uche Ibute <uchennaoluchi49@gmail.com>
2022-12-15 22:53:24 +0100

    first commit

---
+++ anagram-checker.js
@@ -0,0 +1,22 @@
+module.exports = function (str) {
+  const stem = {}
+  for(let i of str.replace(/[^\w]/g).toLowerCase()) {
+    stem[i] = stem[i] + 1 || 1
+  }
+  return stem
+}
+const anagram = (first, second) => {
+  const astem = buildCharObject(first)
+  const bstem = buildCharObject(second)
+
+  if(Object.keys(astem).length !== Object.keys(bstem).length) {
+    return false
+  }
+  for(let v in astem) {
+    if(astem[v] !== bstem[v]) {
+      return false
+    }
+  }
+    return true
+}
+
